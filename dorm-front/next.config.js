a54971e5e182a59eb71d897dcf3f0a43bb0e016b
ummiih <156565796+hwangyummii@users.noreply.github.com>
2024-06-29 20:45:14 +0900

    chore: next.config.js 사진 domain추가

--- dorm-front/next.config.js
+++ dorm-front/next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["github.com", "k.kakaocdn.net", "storage.googleapis.com"],
+    domains: [
+      "github.com",
+      "k.kakaocdn.net",
+      "storage.googleapis.com",
+      "dormitory-family-images-bucket.s3.ap-northeast-2.amazonaws.com",
+      "t1.kakaocdn.net",
+    ],
   },
   reactStrictMode: false,
   swcMinify: true,
