8ad5aa2bce133699a1dec8ea664a65175ccd59f8
ummiih <156565796+hwangyummii@users.noreply.github.com>
2024-07-01 20:27:06 +0900

    feat: Navbar, Header

--- dorm-front/src/app/(main)/home/page.tsx
+++ dorm-front/src/app/(main)/home/page.tsx
@@ -1,35 +1,43 @@
 "use client";
 
 import Button from "@/components/common/Button";
+import Header from "@/components/common/Header";
+import NavBar from "@/components/common/NavBar";
 import HomeBanner from "@/components/home/HomeBanner";
 import HomeMenu from "@/components/home/HomeMenu";
 import HomePost from "@/components/home/HomePost";
 
 const Home = () => {
   return (
-    <div className="bg-background mx-5">
-      <div className="h-[33px]" />
-      <div className="flex flex-col gap-y-8">
-        <div>
-          <div className="flex justify-between">
-            <div className="text-black text-h3 font-bold">오늘의 메뉴</div>
-            <Button className={"home-button"} Icon={ArrowIcon}>
-              전체보기
-            </Button>
+    <div>
+      <Header></Header>
+      <div className={"h-[52px]"}></div>
+      <div className="bg-background mx-5">
+        <div className="h-[33px]" />
+        <div className="flex flex-col gap-y-8">
+          <div>
+            <div className="flex justify-between">
+              <div className="text-black text-h3 font-bold">오늘의 메뉴</div>
+              <Button className={"home-button"} Icon={ArrowIcon}>
+                전체보기
+              </Button>
+            </div>
+            <HomeMenu />
           </div>
-          <HomeMenu />
-        </div>
-        <HomeBanner />
-        <div>
-          <div className="flex justify-between">
-            <div className="text-black text-h3 font-bold">인기 게시글</div>
-            <Button className={"home-button"} Icon={ArrowIcon}>
-              전체보기
-            </Button>
+          <HomeBanner />
+          <div>
+            <div className="flex justify-between">
+              <div className="text-black text-h3 font-bold">인기 게시글</div>
+              <Button className={"home-button"} Icon={ArrowIcon}>
+                전체보기
+              </Button>
+            </div>
+            <HomePost />
           </div>
-          <HomePost />
         </div>
       </div>
+      <div className={"h-[80px]"} />
+      <NavBar />
     </div>
   );
 };
@@ -37,13 +45,7 @@ export default Home;
 
 function ArrowIcon(props: React.SVGProps<SVGSVGElement>) {
   return (
-    <svg
-      width={13}
-      height={11}
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      {...props}
-    >
+    <svg width={13} height={11} fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
       <path
         d="M11.171 5.64H1.313M11.701 5.64l-4-5M8.606 9.303l3.095-3.663"
         stroke="#FF7E8D"
