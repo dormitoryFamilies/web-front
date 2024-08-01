f27e079a25433bc1b05f335e7b075a6d6e76766c
ummiih <156565796+hwangyummii@users.noreply.github.com>
2024-08-01 18:16:46 +0900

    feat: 체질 상태 입력 기능 구현 #1

--- dorm-front/src/components/room-mate/Constitution.tsx
+++ dorm-front/src/components/room-mate/Constitution.tsx
@@ -1,73 +1,96 @@
-import React, { useState } from "react";
-import Header from "@/components/room-mate/Header";
 import Image from "next/image";
+import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
+
+import Header from "@/components/room-mate/Header";
 import Item from "@/components/room-mate/Item";
-import { HeatToleranceType } from "@/types/room-mate/type";
+import RequirementBanner from "@/components/room-mate/RequirementBanner";
+import { lifeStylePostAtom } from "@/recoil/room-mate/atom";
+import { HeatToleranceType, RoomMateLifeStyleStepType } from "@/types/room-mate/type";
+import { coldToleranceContents, heatToleranceContents } from "@/utils/room-mate/lifestyles";
 interface Props {
-  onNext: React.Dispatch<React.SetStateAction<string>>;
-  onBefore?: React.Dispatch<React.SetStateAction<string>>;
+  setStep: Dispatch<SetStateAction<RoomMateLifeStyleStepType>>;
 }
 const Constitution = (props: Props) => {
-  const { onNext, onBefore } = props;
-  const [clickedHeatIndex, setClickedHeatIndex] = useState<HeatToleranceType>("");
-  const [clickedColdIndex, setClickedColdIndex] = useState<HeatToleranceType>("");
+  const { setStep } = props;
+  const [lifeStylePostData, setLifeStylePostData] = useRecoilState(lifeStylePostAtom);
+  const [heatTolerance, setHeatTolerance] = useState<HeatToleranceType>("");
+  const [coldTolerance, setColdTolerance] = useState<HeatToleranceType>("");
 
-  const heatIndex = ["적게 탐", "조금 탐", "많이 탐"];
-  const coldIndex = ["적게 탐", "조금 탐", "많이 탐"];
+  useEffect(() => {
+    console.log(lifeStylePostData);
+  }, [lifeStylePostData]);
+
+  const handleNextClick = () => {
+    setLifeStylePostData((prevState) => ({
+      ...prevState,
+      heatTolerance: heatTolerance,
+      coldTolerance: coldTolerance,
+    }));
+    setStep("MBTI");
+  };
 
   return (
-    <div className={"flex flex-col p-5"}>
+    <>
       <Header />
-      <div className={"flex flex-col gap-y-4 relative justify-center items-center"}>
-        <div className={"flex flex-col gap-y-2"}>
-          <div className={"flex justify-center"}>
-            <div className={"text-gray5"}>4 / 10</div>
-          </div>
+      <RequirementBanner />
+      <div className={"flex flex-col p-5"}>
+        <div className={"flex flex-col gap-y-4 relative justify-center items-center"}>
+          <div className={"flex flex-col gap-y-2"}>
+            <div className={"flex justify-center"}>
+              <div className={"text-gray5"}>4 / 10</div>
+            </div>
 
-          <div className={"flex items-center justify-center"}>
-            <div className={"absolute w-[148px] h-1 bg-gray1 rounded-full "}>
-              <div className={"absolute w-[60px] h-1 rounded-full bg-primaryMid"}></div>
+            <div className={"flex items-center justify-center"}>
+              <div className={"absolute w-[148px] h-1 bg-gray1 rounded-full "}>
+                <div className={"absolute w-[60px] h-1 rounded-full bg-primaryMid"}></div>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className={"flex flex-col items-center justify-center"}>
-          <div className={"relative w-[200px] h-[140px]"}>
-            <Image
-              src={"/room-mate/추위,_더위.png"}
-              alt={"/room-mate/추위,_더위.png"}
-              className={"absolute object-cover"}
-              fill
-            />
+          <div className={"flex flex-col items-center justify-center"}>
+            <div className={"relative w-[200px] h-[140px]"}>
+              <Image
+                src={"/room-mate/추위,_더위.png"}
+                alt={"/room-mate/추위,_더위.png"}
+                className={"absolute object-cover"}
+                fill
+              />
+            </div>
           </div>
+          <div className={"text-h3 font-semibold"}>나의 체질은?</div>
         </div>
-        <div className={"text-h3 font-semibold"}>나의 체질은?</div>
-      </div>
 
-      <div className={"flex flex-col gap-y-[28px] mt-[32px]"}>
-        <Item
-          title={"더위"}
-          contents={heatIndex}
-          className={"grid-cols-4"}
-          setIsClickedItem={setClickedHeatIndex}
-          selectedContent={clickedHeatIndex}
-        />
-        <Item
-          title={"추위"}
-          contents={coldIndex}
-          className={"grid-cols-4"}
-          setIsClickedItem={setClickedColdIndex}
-          selectedContent={clickedColdIndex}
-        />
-        <button
-          onClick={onNext}
-          className={
-            "w-full rounded-full bg-gray3 text-white text-h5 py-4 hover:bg-primary hover:text-white transition"
-          }>
-          다음
-        </button>
+        <div className={"flex flex-col gap-y-[28px] mt-[32px]"}>
+          <Item
+            title={"더위"}
+            isRequired={true}
+            contents={heatToleranceContents}
+            className={"grid-cols-3"}
+            selectedContent={heatTolerance}
+            setSelectedContent={setHeatTolerance}
+          />
+          <Item
+            title={"추위"}
+            isRequired={true}
+            contents={coldToleranceContents}
+            className={"grid-cols-3"}
+            selectedContent={coldTolerance}
+            setSelectedContent={setColdTolerance}
+          />
+          <button
+            disabled={coldTolerance === "" || heatTolerance === ""}
+            onClick={handleNextClick}
+            className={
+              coldTolerance === "" || heatTolerance === ""
+                ? "w-full rounded-full bg-gray3 text-white text-h5 py-4 hover:text-white transition"
+                : "w-full rounded-full bg-primary text-white text-h5 py-4 hover:text-white transition"
+            }>
+            다음
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 export default Constitution;
