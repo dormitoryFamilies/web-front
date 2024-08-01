00a6464933e48fb11e16a05953b7e9e61478b22a
ummiih <156565796+hwangyummii@users.noreply.github.com>
2024-08-01 16:05:09 +0900

    chore: clean up code #1

--- dorm-front/src/app/(main)/room-mate/page.tsx
+++ dorm-front/src/app/(main)/room-mate/page.tsx
@@ -1,14 +1,13 @@
 "use client";
 
-import Image from "next/image";
 import { useRouter } from "next/navigation";
 import * as React from "react";
 import { SVGProps, useState } from "react";
+
 import RoommateMatchEditor from "@/components/room-mate/RoommateMatchEditor";
 
 const RoommateMatching = () => {
   const router = useRouter();
-
   return (
     <>
       <RoommateMatchEditor />
