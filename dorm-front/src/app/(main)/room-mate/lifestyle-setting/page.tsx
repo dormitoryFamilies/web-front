61a31e215279c2ee9bc6b9cdf5d089838d61ba90
ummiih <156565796+hwangyummii@users.noreply.github.com>
2024-08-05 17:54:08 +0900

    chore: Editor를 컴포넌트에서 페이지로 변경 #1

---
+++ dorm-front/src/app/(main)/room-mate/lifestyle-setting/page.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+import Constitution from "@/components/room-mate/Constitution";
+import CycleToReturnHome from "@/components/room-mate/CycleToReturnHome";
+import Done from "@/components/room-mate/Done";
+import Exam from "@/components/room-mate/Exam";
+import Food from "@/components/room-mate/Food";
+import LifeStyle from "@/components/room-mate/LifeStyle";
+import MBTI from "@/components/room-mate/MBTI";
+import OtherLifestyles from "@/components/room-mate/OtherLifestyles";
+import SleepPattern from "@/components/room-mate/SleepPattern";
+import SmokingAndDrinking from "@/components/room-mate/SmokingAndDrinking";
+import SoundAndPerfume from "@/components/room-mate/SoundAndPerfume";
+import { RoomMateLifeStyleStepType } from "@/types/room-mate/type";
+const LifeStyleEditor = () => {
+  const [lifeStyleStep, setLifeStyleStep] = useState<RoomMateLifeStyleStepType>("SleepPattern");
+
+  return (
+    <div>
+      {lifeStyleStep === "SleepPattern" && <SleepPattern setLifeStyleStep={setLifeStyleStep} />}
+      {lifeStyleStep === "SmokingDrinking" && <SmokingAndDrinking setLifeStyleStep={setLifeStyleStep} />}
+      {lifeStyleStep === "LifeStyle" && <LifeStyle setLifeStyleStep={setLifeStyleStep} />}
+      {lifeStyleStep === "Constitution" && <Constitution setLifeStyleStep={setLifeStyleStep} />}
+      {lifeStyleStep === "MBTI" && <MBTI setLifeStyleStep={setLifeStyleStep} />}
+      {lifeStyleStep === "CycleToReturnHome" && <CycleToReturnHome setLifeStyleStep={setLifeStyleStep} />}
+      {lifeStyleStep === "Food" && <Food setLifeStyleStep={setLifeStyleStep} />}
+      {lifeStyleStep === "SoundAndPerfume" && <SoundAndPerfume setLifeStyleStep={setLifeStyleStep} />}
+      {lifeStyleStep === "Exam" && <Exam setLifeStyleStep={setLifeStyleStep} />}
+      {lifeStyleStep === "OtherLifestyles" && <OtherLifestyles setLifeStyleStep={setLifeStyleStep} />}
+      {lifeStyleStep === "Done" && <Done setLifeStyleStep={setLifeStyleStep} />}
+    </div>
+  );
+};
+export default LifeStyleEditor;
