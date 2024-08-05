5e5e37903e04443914f8c34708ddbe025a313b9d
ummiih <156565796+hwangyummii@users.noreply.github.com>
2024-08-05 17:49:26 +0900

    feat: 소리 및 향수 라이프스타일 기능 설정 구현 #1

--- dorm-front/src/components/room-mate/SoundAndPerfume.tsx
+++ dorm-front/src/components/room-mate/SoundAndPerfume.tsx
@@ -1,72 +1,91 @@
-import Header from "@/components/room-mate/Header";
 import Image from "next/image";
+import React, { Dispatch, SetStateAction, useState } from "react";
+import { useRecoilState } from "recoil";
+
+import Header from "@/components/room-mate/Header";
 import Item from "@/components/room-mate/Item";
-import React, { useState } from "react";
-import { PerfumeUsageType, PhoneSoundType } from "@/types/room-mate/type";
+import RequirementBanner from "@/components/room-mate/RequirementBanner";
+import { lifeStylePostAtom } from "@/recoil/room-mate/atom";
+import { PerfumeUsageType, PhoneSoundType, RoomMateLifeStyleStepType } from "@/types/room-mate/type";
+import { perfumeUsageContents, phoneSoundContents } from "@/utils/room-mate/lifestyles";
 interface Props {
-  onNext: React.Dispatch<React.SetStateAction<string>>;
-  onBefore?: React.Dispatch<React.SetStateAction<string>>;
+  setLifeStyleStep: Dispatch<SetStateAction<RoomMateLifeStyleStepType>>;
 }
 const SoundAndPerfume = (props: Props) => {
-  const { onNext, onBefore} = props;
-  const [clickedPhoneSound, setClickedPhoneSound] = useState<PhoneSoundType>("");
-  const [clickedPerfume, setClickedPerfume] = useState<PerfumeUsageType>("");
+  const { setLifeStyleStep } = props;
+  const [lifeStylePostData, setLifeStylePostData] = useRecoilState(lifeStylePostAtom);
+  const [phoneSound, setPhoneSound] = useState<PhoneSoundType>("");
+  const [perfumeUsage, setPerfumeUsage] = useState<PerfumeUsageType>("");
+  const handleNextClick = () => {
+    setLifeStylePostData((prevState) => ({
+      ...prevState,
+      ...(phoneSound !== "" && { phoneSound: phoneSound }),
+      perfumeUsage: perfumeUsage,
+    }));
+    setLifeStyleStep("Exam");
+  };
 
-  const phoneSound = ["이어폰", "스피커", "유동적"];
-  const perfume = ["미사용", "가끔", "자주"];
   return (
-    <div className={"flex flex-col p-5"}>
+    <>
       <Header />
-      <div className={"flex flex-col gap-y-4 relative justify-center items-center"}>
-        <div className={"flex flex-col gap-y-2"}>
-          <div className={"flex justify-center"}>
-            <div className={"text-gray5"}>8 / 10</div>
-          </div>
+      <RequirementBanner />
+      <div className={"flex flex-col p-5"}>
+        <div className={"flex flex-col gap-y-4 relative justify-center items-center"}>
+          <div className={"flex flex-col gap-y-2"}>
+            <div className={"flex justify-center"}>
+              <div className={"text-gray5"}>8 / 10</div>
+            </div>
 
-          <div className={"flex items-center justify-center"}>
-            <div className={"absolute w-[148px] h-1 bg-gray1 rounded-full "}>
-              <div className={"absolute w-[120px] h-1 rounded-full bg-primaryMid"}></div>
+            <div className={"flex items-center justify-center"}>
+              <div className={"absolute w-[148px] h-1 bg-gray1 rounded-full "}>
+                <div className={"absolute w-[120px] h-1 rounded-full bg-primaryMid"}></div>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className={"flex flex-col items-center justify-center"}>
-          <div className={"relative w-[200px] h-[140px]"}>
-            <Image
-              src={"/room-mate/휴대폰_소리.png"}
-              alt={"/room-mate/휴대폰_소리.png"}
-              className={"absolute object-cover"}
-              fill
-            />
+          <div className={"flex flex-col items-center justify-center"}>
+            <div className={"relative w-[200px] h-[140px]"}>
+              <Image
+                src={"/room-mate/휴대폰_소리.png"}
+                alt={"/room-mate/휴대폰_소리.png"}
+                className={"absolute object-cover"}
+                fill
+              />
+            </div>
           </div>
+          <div className={"text-h3 font-semibold"}>나는 주로 어떤 편인가요?</div>
         </div>
-        <div className={"text-h3 font-semibold"}>나는 주로 어떤 편인가요?</div>
-      </div>
 
-      <div className={"flex flex-col gap-y-[28px] mt-[32px]"}>
-        <Item
-          title={"휴대폰 소리"}
-          contents={phoneSound}
-          className={"grid-cols-3"}
-          setIsClickedItem={setClickedPhoneSound}
-          selectedContent={clickedPhoneSound}
-        />
-        <Item
-          title={"향수"}
-          contents={perfume}
-          className={"grid-cols-3"}
-          setIsClickedItem={setClickedPerfume}
-          selectedContent={clickedPerfume}
-        />
-        <button
-          onClick={onNext}
-          className={
-            "w-full rounded-full bg-gray3 text-white text-h5 py-4 hover:bg-primary hover:text-white transition"
-          }>
-          다음
-        </button>
+        <div className={"flex flex-col gap-y-[28px] mt-[32px]"}>
+          <Item
+            title={"휴대폰 소리"}
+            isRequired={false}
+            contents={phoneSoundContents}
+            className={"grid-cols-3"}
+            setSelectedContent={setPhoneSound}
+            selectedContent={phoneSound}
+          />
+          <Item
+            title={"향수"}
+            isRequired={true}
+            contents={perfumeUsageContents}
+            className={"grid-cols-3"}
+            setSelectedContent={setPerfumeUsage}
+            selectedContent={perfumeUsage}
+          />
+          <button
+            disabled={perfumeUsage === ""}
+            onClick={handleNextClick}
+            className={
+              perfumeUsage === ""
+                ? "absolute bottom-5 left-5 w-[90%] rounded-full bg-gray3 text-white text-h5 py-4 hover:text-white transition"
+                : "absolute bottom-5 left-5 w-[90%] rounded-full bg-primary text-white text-h5 py-4 hover:text-white transition"
+            }>
+            다음
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 export default SoundAndPerfume;
