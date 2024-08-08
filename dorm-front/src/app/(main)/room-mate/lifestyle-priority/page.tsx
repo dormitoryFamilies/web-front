0848d11503c57846675de960d1d2986b51ad1179
ummiih <156565796+hwangyummii@users.noreply.github.com>
2024-08-09 01:33:45 +0900

    feat: 우선순위 설정 페이지 라우팅 #1

---
+++ dorm-front/src/app/(main)/room-mate/lifestyle-priority/page.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/common/Header";
+
+const RoommateLifeStylePriority = () => {
+  return (
+    <>
+      <Header title={"룸메 우선순위설정"} />
+
+    </>
+  )
+}
+export default RoommateLifeStylePriority;
