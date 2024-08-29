c857a66b0b6879adb5b3001795597849f4c101de
ummiih <156565796+hwangyummii@users.noreply.github.com>
2024-08-29 15:08:15 +0900

    feat: 룸메 매칭 신청 컴포넌트 #7

---
+++ dorm-front/src/components/room-mate/RoommateMatchSuccess.tsx
@@ -0,0 +1,70 @@
+import * as React from "react";
+
+import Header from "@/components/common/Header";
+import RoommateMatchCard from "@/components/room-mate/RoommateMatchCard";
+
+interface Props {
+  memberId: number;
+  setIsRoommateMatchSuccessOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  setIsConfirmRoommateMatchCancelOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+const RoommateMatchSuccess = (props: Props) => {
+  const { memberId, setIsRoommateMatchSuccessOpen, setIsConfirmRoommateMatchCancelOpen } = props;
+
+  const onBack = () => {
+    setIsRoommateMatchSuccessOpen(false);
+  };
+
+  const handleCancelClick = () => {
+    setIsRoommateMatchSuccessOpen(false);
+    setIsConfirmRoommateMatchCancelOpen(true);
+  };
+
+  const handleCloseClick = () => {
+    setIsRoommateMatchSuccessOpen(false);
+  };
+
+  const topElement = () => {
+    return <div className={"px-3 py-1 text-h5 bg-gray0 rounded-full w-fit"}>받는 분의 프로필</div>;
+  };
+
+  return (
+    <div className={"flex flex-col min-h-screen"}>
+      <Header headerType={"dynamic"} title={"룸메 매칭 신청"} onBack={onBack} />
+      <div className={"h-[60px]"} />
+      <div>
+        <div className={"px-5 mt-6"}>
+          {/* 안내문구 */}
+          <div className={"mb-6"}>
+            <div className={"text-h2 font-semibold"}>
+              닉네임<span className={"text-h4 font-normal"}>님과의</span> <br />
+              매칭 신청 수락을 기다리고 있어요{" "}
+            </div>
+            <div className={""}>
+              <div className={"text-h5 text-gray4 mt-1"}>닫기를 눌러도 매칭 신청이 취소되지 않아요.</div>
+            </div>
+          </div>
+          {/* 카드 */}
+          <RoommateMatchCard memberId={memberId} topElement={topElement} bottomButton={false} />
+        </div>
+      </div>
+      <div className={"mt-auto mx-5 flex gap-x-3 mb-5"}>
+        <button
+          onClick={() => {
+            handleCloseClick();
+          }}
+          className={"w-full rounded-full bg-gray1 text-h5 text-gray5 py-4"}>
+          닫기
+        </button>
+        <button
+          onClick={() => {
+            handleCancelClick();
+          }}
+          className={"w-full rounded-full bg-white text-h5 text-gray5 border-[1px] border-gray1 py-4"}>
+          신청 취소
+        </button>
+      </div>
+    </div>
+  );
+};
+export default RoommateMatchSuccess;
