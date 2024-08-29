10236d87883e1a13c5adbd7058e691110127e2f5
ummiih <156565796+hwangyummii@users.noreply.github.com>
2024-08-29 15:04:30 +0900

    feat: 룸메 추천 인원 #7

---
+++ dorm-front/src/components/room-mate/RoommateMatchList.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import { useRecoilState } from "recoil";
+
+import Header from "@/components/common/Header";
+import RoommateMatchListProfile from "@/components/room-mate/RoommateMatchListProfile";
+import { candidateIdsAtom } from "@/recoil/room-mate/atom";
+
+interface Props {
+  setIsRoommateMatchListOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+const RoommateMatchList = (props: Props) => {
+  const [candidateIds, setCandidateIds] = useRecoilState(candidateIdsAtom);
+
+  return (
+    <>
+      <Header headerType={"dynamic"} title={"전체 추천 목록"} />
+      <div className={"h-[60px]"}></div>
+      <div className={"flex flex-col gap-y-3 px-5 mt-6"}>
+        {candidateIds.map((candidateId) => {
+          return <RoommateMatchListProfile memberId={candidateId} key={candidateId}></RoommateMatchListProfile>;
+        })}
+      </div>
+    </>
+  );
+};
+export default RoommateMatchList;
