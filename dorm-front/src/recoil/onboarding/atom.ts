1c956287c06ba4b6a636b117cd2c4073a1f262bd
유림 <yulimh001@gmail.com>
2024-12-06 12:06:48 +0900

    fix: atom 값 초기화 #12

--- dorm-front/src/recoil/onboarding/atom.ts
+++ dorm-front/src/recoil/onboarding/atom.ts
@@ -5,11 +5,11 @@ import { ProfileSettingType } from "@/types/global";
 export const profileSettingAtom = atom<ProfileSettingType>({
   key: "profileSettingAtom",
   default: {
-    nickname: "유림",
-    studentCardImageUrl: "https://k.kakaocdn.net/dn/G4MPH/btrfw4guzVh/iXmuhGiV1QRMsZkMsKRrc1/img_640x640.jpg",
+    nickname: "",
+    studentCardImageUrl: "",
     collegeType: "단과대학교",
     departmentType: "학과선택",
     studentNumber: 0,
-    dormitoryType: "본관",
+    dormitoryType: "",
   },
 });
