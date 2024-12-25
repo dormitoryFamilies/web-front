cb9d1e90a3f9b5817b0662c7076dd841c8bd283d
유림 <yulimh001@gmail.com>
2024-12-25 20:24:26 +0900

    feat: 보낸 룸메 신청 페이지 구현 #14

--- dorm-front/src/components/room-mate/ ApplicationSentList.tsx
+++ dorm-front/src/components/room-mate/ ApplicationSentList.tsx
@@ -1,8 +1,66 @@
+import { useEffect, useState } from "react";
+import * as React from "react";
+
+import Paging from "@/components/common/Paging";
+import ApplicationSentProfile from "@/components/room-mate/ApplicationSentProfile";
+import useMatchingRequests from "@/lib/hooks/useMatchingRequests";
+
 const ApplicationSentList = () => {
+  const [pageNumber, setPageNumber] = useState<number>(0);
+  const { matchingRequests, mutate } = useMatchingRequests("sent", pageNumber);
+
+  useEffect(() => {
+    console.log("matchingRequests", matchingRequests);
+  }, [matchingRequests]);
+
+  // 팔로우 페이징 이전 버튼 클릭시
+  const handlerBeforePageNumber = () => {
+    if (pageNumber === 0) {
+      setPageNumber(0);
+    } else {
+      setPageNumber(pageNumber - 1);
+    }
+  };
+
+  // 팔로우 페이징 다음 버튼 클릭시
+  const handlerNextPageNumber = () => {
+    if (matchingRequests && pageNumber >= matchingRequests[0]?.data.data.totalPageNumber - 1) {
+      setPageNumber(matchingRequests[0]?.data.data.totalPageNumber - 1);
+    } else {
+      setPageNumber(pageNumber + 1);
+    }
+  };
+
   return (
-    <div>
-dfs
-    </div>
+    <main className={"px-5"}>
+      <section className={"mt-[22px] flex flex-col gap-y-2"}>
+        {matchingRequests &&
+          matchingRequests.map((matchingRequest) => {
+            return matchingRequest.data.data.memberProfiles.map((memberProfile) => {
+              return (
+                <ApplicationSentProfile
+                  key={memberProfile.memberId}
+                  memberId={memberProfile.memberId}
+                  profileUrl={memberProfile.profileUrl}
+                  nickname={memberProfile.nickname}
+                  mutate={mutate}
+                />
+              );
+            });
+          })}
+      </section>
+
+      {/* 페이징 */}
+      <section>
+        <Paging
+          setPageNumber={setPageNumber}
+          handlerNextButton={handlerNextPageNumber}
+          pageNumber={pageNumber}
+          totalPageNumber={matchingRequests && matchingRequests[0]?.data.data.totalPageNumber}
+          handlerBeforeButton={handlerBeforePageNumber}
+        />
+      </section>
+    </main>
   );
 };
 export default ApplicationSentList;
