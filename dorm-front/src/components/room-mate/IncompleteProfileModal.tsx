3c7581b6b606cae6a81b97b74ed96ef425708d0d
유림 <yulimh001@gmail.com>
2025-01-03 06:46:28 +0900

    style: 모달 배경 조금 더 어둡게 #37

--- dorm-front/src/components/room-mate/IncompleteProfileModal.tsx
+++ dorm-front/src/components/room-mate/IncompleteProfileModal.tsx
@@ -14,7 +14,7 @@ const IncompleteProfileModal = (props: Props) => {
   return (
     <div
       className={
-        "absolute left-0 right-0 z-50 flex flex-col gap-y-2 justify-center bg-[rgba(0,0,0,0.2)] px-8 min-h-screen"
+        "absolute left-0 right-0 z-50 flex flex-col gap-y-2 justify-center bg-[rgba(0,0,0,0.5)] px-8 min-h-screen"
       }>
       <section className={"flex flex-col gap-y-8 justify-center items-center bg-white py-8 px-7 rounded-[32px]"}>
         <div className={"flex flex-col gap-y-3"}>
