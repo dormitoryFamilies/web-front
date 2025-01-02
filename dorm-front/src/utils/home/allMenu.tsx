ae7e8fd438d3d872eccd397d86ef8f050efa22f9
유림 <yulimh001@gmail.com>
2025-01-03 07:55:36 +0900

    refactor: utils 타입 수정, 사용하지 않는 파일 제거 #39

--- dorm-front/src/utils/home/allMenu.tsx
+++
@@ -1,7 +0,0 @@
-import { FoodMenuList } from "@/types/global";
-
-export const ALL_MENU: FoodMenuList = {
-  breakfast: ["잡곡밥", "매운버섯찌개", "참치김치볶음", "연두부*양념장", "조미구이김", "색우유or두유"],
-  lunch: ["쑥갓유부우동", "김가루밥", "아쿠아돈까스", "콘샐러드", "단무지", "포기김치"],
-  dinner: ["잡곡밥", "청국장", "돈육가래떡찜", "아삭이쌈장무침", "봄동겉절이"],
-};
