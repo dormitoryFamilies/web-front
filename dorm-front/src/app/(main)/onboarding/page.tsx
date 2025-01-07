6941891a38c4d221504bc27640fb30f5bbabe7a7
유림 <yulimh001@gmail.com>
2025-01-07 11:58:58 +0900

    fix: role data구조 변경

--- dorm-front/src/app/(main)/onboarding/page.tsx
+++ dorm-front/src/app/(main)/onboarding/page.tsx
@@ -23,8 +23,8 @@ const OnBoarding = () => {
               Cookies.set("accessToken", res.headers.accessToken, { expires: Date.now() + 604800000 });
               Cookies.set("refreshToken", res.headers.refreshToken, { expires: Date.now() + 604800000 });
               getUserRole().then((r) => {
-                console.log("r", r.data.data);
-                Cookies.set("role", "ROLE_ADMIN");
+                console.log("r", r.data);
+                Cookies.set("role", r.data.authority);
               });
             }
           });
