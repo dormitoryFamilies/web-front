4b46f67f8f8cf27481d9409eef14f969502a3ea3
유림 <yulimh001@gmail.com>
2025-01-11 22:38:25 +0900

    style: 필터 위치 변경을 위한 twMerge 추가 #51

--- dorm-front/src/components/board/ArticleStatusFilter.tsx
+++ dorm-front/src/components/board/ArticleStatusFilter.tsx
@@ -1,18 +1,22 @@
+import { twMerge } from "tailwind-merge";
+
 import { BoardStatusType } from "@/types/board/type";
 
 interface Props {
+  className?: string;
   statusFilterContents: BoardStatusType[];
   setSelectedStatusType: React.Dispatch<React.SetStateAction<BoardStatusType>>;
   setIsStatusFilterClick: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 const ArticleStatusFilter = (props: Props) => {
-  const { statusFilterContents, setSelectedStatusType, setIsStatusFilterClick } = props;
+  const { className, statusFilterContents, setSelectedStatusType, setIsStatusFilterClick } = props;
   return (
     <div
-      className={
-        "flex flex-col gap-y-2 absolute w-[80px] top-8 bg-white border-[1px] border-gray1 rounded-[16px] py-2"
-      }>
+      className={twMerge(
+        "flex flex-col gap-y-2 absolute w-[80px] top-8 bg-white border-[1px] border-gray1 rounded-[16px] py-2",
+        className,
+      )}>
       {statusFilterContents.map((statusFilterContent, index) => {
         return (
           <div
